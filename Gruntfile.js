40af98c2f064bb545ec310c9969f6424448efd40
Daniel Furze <daniel@furzeface.com>
2014-09-30 07:53:12 +0100

    chore(Grunt): Tidies up Grunt build proccess

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,11 @@
  * Copyright (c) 2014 Building Blocks
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 module.exports = function(grunt) {
+  // Load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
   // Project configuration.
   grunt.initConfig({
     jshint: {
@@ -49,10 +50,6 @@ module.exports = function(grunt) {
   });
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', [
