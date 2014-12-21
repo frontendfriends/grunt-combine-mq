4b70afec21f995c608142b34a648c605ccff7d9a
Daniel Furze <daniel@furzeface.com>
2014-12-21 17:40:40 +0000

    feat(Task): Makes CSS beautify options, on by default

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@
 module.exports = function(grunt) {
   // Load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     jshint: {
@@ -22,18 +23,21 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
+
     // Before generating any new files, remove any previously-created files.
     clean: {
       tests: [
       'tmp'
       ]
     },
+
     // Unit tests.
     nodeunit: {
       tests: [
       'test/*_test.js'
       ]
     },
+
     // Configuration to be run (and then tested).
     combine_mq: {
       default_options: {
@@ -43,20 +47,25 @@ module.exports = function(grunt) {
         dest: 'test/actual/'
       },
       new_filename: {
+      	options: {
+					beautify: false
+				},
         src: 'test/fixtures/test.css',
-        dest: 'test/actual/new_filename.css'
+        dest: 'test/actual/custom_options.css'
       }
     }
   });
+
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
+
+  // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this plugin's task(s), then test the result.
   grunt.registerTask('test', [
     'clean',
     'combine_mq',
     'nodeunit'
   ]);
+
   // By default, lint and run all tests.
   grunt.registerTask('default', [
     'jshint',
--- tasks/combine_mq.js
+++ tasks/combine_mq.js
@@ -5,19 +5,30 @@
  * Copyright (c) 2014 Building Blocks
  * Licensed under the MIT license.
  */
+
 'use strict';
+
 module.exports = function (grunt) {
   grunt.registerMultiTask('combine_mq', 'Grunt wrapper for node-combine-mq', function() {
     var combineMq = require('combine-mq');
-    var options = this.options({});
-    this.files.forEach( function (file, next) {
-      var src = file.src[0];
-      var dest = file.dest;
+
+    var options = this.options({
+    	beautify: true
+    });
+
+    this.files.forEach(function (file, next) {
+      var src = file.src[0],
+      dest = file.dest,
+      processed;
+
       if (!grunt.file.exists(src)) {
         grunt.log.warn('Source file "' + src + '" not found.');
+
         return next();
       }
-      var processed = combineMq.parseCssString(grunt.file.read(src), options);
+
+      processed = combineMq.parseCssString(grunt.file.read(src), options);
+
       grunt.file.write(file.dest, processed);
       grunt.log.writeln('File "' + file.dest + '" created.');
     });
